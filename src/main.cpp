c039ce2e88742a6fe58a171385b4350070fd636d
u-tanick <funkybuffalo.59@gmail.com>
2024-11-03 00:25:38 +0900

    update

---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+#include <M5Unified.h>
+#include "Kalman.h"
+
+//Filter
+Kalman kalmanX;
+Kalman kalmanY;
+
+// IMU paramater
+float gyroX, gyroY, gyroZ;
+float accX, accY, accZ;
+
+float angleX, angleY;
+float gyroRateX, gyroRateY;
+unsigned long lastUpdate;
+
+float filteredAngleX;
+float filteredAngleY;
+
+float originAngleX = 0.0;
+float originAngleY = 0.0;
+
+float relativeAngleX;
+float relativeAngleY;
+
+void calculateAngle() {
+  // 時間経過を取得
+  unsigned long now = millis();
+  float dt = (now - lastUpdate) / 1000.0; // 秒単位の時間経過
+  lastUpdate = now;
+
+  // 加速度から角度を計算
+  angleX = atan2(accY, accZ) * 180 / PI;
+  angleY = atan2(accX, accZ) * 180 / PI;
+
+  // ジャイロデータを使って角速度を取得
+  gyroRateX = gyroX; // ジャイロの出力がdeg/sであることを前提
+  gyroRateY = gyroY;
+
+  // カルマンフィルターを使って角度を更新
+  // オフセット値を加算することでボタンを押した時点を原点とした数値に補正
+  filteredAngleX = kalmanX.getAngle(angleX, gyroRateX, dt);
+  filteredAngleY = kalmanY.getAngle(angleY, gyroRateY, dt);
+}
+
+void resetOrigin() {
+    // 原点を引いて相対角度を算出
+    relativeAngleX = filteredAngleX - originAngleX;
+    relativeAngleY = filteredAngleY - originAngleY;
+}
+
+void setup() {
+  // M5StickC Plusの初期化
+  auto cfg = M5.config();
+  cfg.internal_imu = true;  // 内蔵IMUを有効化
+  M5.begin(cfg);
+  M5.Imu.init();
+  
+  // 画面の設定
+  M5.Lcd.begin();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(2);
+
+  lastUpdate = millis();
+}
+
+void loop() {
+
+  // ジャイロデータを取得
+  M5.Imu.getGyroData(&gyroX, &gyroY, &gyroZ);
+  M5.Imu.getAccelData(&accX,&accY,&accZ);
+
+  // カルマンフィルタを使用して角度を計算
+  calculateAngle();
+
+  // 画面に表示する前にクリア
+  M5.Lcd.fillScreen(BLACK);
+
+  // ホームボタンを押したか？（1度だけ取得可能）
+  if ( M5.BtnA.wasPressed() ) {
+    M5.Speaker.tone(1500, 100);
+    // ボタンを押した時点の角度を原点として扱うために取得
+    originAngleX = filteredAngleX;
+    originAngleY = filteredAngleY;
+  }
+
+  // ボタンを押した時点の originAngleX, originAngleY を使用して原点をリセット
+  resetOrigin();
+
+  // 縦置きで前後回転方向
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.printf("Angle X: %d\n", int(filteredAngleX));
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.printf("Angle X: %d\n", int(relativeAngleX));
+
+  // 縦置きで左右回転方向
+  M5.Lcd.setCursor(0, 70);
+  M5.Lcd.printf("Angle Y: %d\n", int(filteredAngleY));
+  M5.Lcd.setCursor(0, 90);
+  M5.Lcd.printf("Angle Y: %d\n", int(relativeAngleY));
+
+  M5.update(); // ボタンの状態を更新
+  delay(10);
+}
