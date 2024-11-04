2e3038303b01a8d6284572f801462e0dcbc0e377
u-tanick <funkybuffalo.59@gmail.com>
2024-11-04 20:24:35 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <M5Unified.h>
 #include <Kalman.h>
+#include <SensorFusion.h>
 
 #if defined(ARDUINO_M5STACK_Core2)	// Core2の場合
   u_int8_t ROTATE = 1;
@@ -15,20 +16,29 @@
   u_int8_t PARAM_POS = 40;
 #endif
 
+// ---------------------------------------------
 // カルマンフィルター用クラス変数
 Kalman kalmanX;
 Kalman kalmanY;
 
+// ---------------------------------------------
+// センサーフュージョン用クラス変数
+SF fusion;
+float deltat;
+
+// ---------------------------------------------
+// 角度算出用変数
 // 角度取得・計算用変数
 float gyroX, gyroY, gyroZ;
 float accX, accY, accZ;
-float angleX, angleY;
 float gyroRateX, gyroRateY;
-unsigned long lastUpdate;
+u_long lastUpdate;
 
-// カルマンフィルターによる補正計算後の角度
-float filteredAngleX;
-float filteredAngleY;
+// X,Y:カルマンフィルターにより計算した角度
+// Z:センサフュージョンにより計算した角度
+float angleX = 0.0;
+float angleY = 0.0;
+float angleZ = 0.0;
 
 // 原点補正のオフセット値
 float originAngleX = 0.0;
@@ -36,45 +46,59 @@ float originAngleY = 0.0;
 float originAngleZ = 0.0;
 
 // 原点補正後の角度
-float relativeAngleX;
-float relativeAngleY;
+float relativeAngleX = 0.0;
+float relativeAngleY = 0.0;
+float relativeAngleZ = 0.0;
 
-// デュアルボタン用ピン情報(Poat.A)
+// GyroZのデータを蓄積するための変数
+float stockedGyroZs[20];
+u_int stockCnt = 0;
+u_int stockedGyroZLength = 0;
+float adjustGyroZ = 0.0;
+
+// ---------------------------------------------
+// デュアルボタン用変数
+// 用ピン情報(Poat.A)
 const uint8_t pin1 = 33;
 const uint8_t pin2 = 32;
+
+int last_value1 = 0;
+int last_value2 = 0;
+
+int cur_value1 = 0;
+int cur_value2 = 0;
+
 
 /**
  * カルマンフィルターを用いた角度計算
  */
 void calculateAngle()
 {
+
   // 時間経過を取得
   unsigned long now = millis();
   float dt = (now - lastUpdate) / 1000.0; // 秒単位の時間経過
   lastUpdate = now;
 
   // 加速度から角度を計算
-  angleX = atan2(accY, accZ) * 180 / PI;
-  angleY = atan2(accX, accZ) * 180 / PI;
-
-  // ジャイロデータを使って角速度を取得
-  gyroRateX = gyroX; // ジャイロの出力がdeg/sであることを前提
-  gyroRateY = gyroY;
+  float pitch = atan2(accY, accZ) * 180 / PI;
+  float roll = atan2(accX, accZ) * 180 / PI;
 
   // カルマンフィルターを使って角度を更新
   // オフセット値を加算することでボタンを押した時点を原点とした数値に補正
-  filteredAngleX = kalmanX.getAngle(angleX, gyroRateX, dt);
-  filteredAngleY = kalmanY.getAngle(angleY, gyroRateY, dt);
+  angleX = kalmanX.getAngle(pitch, gyroX, dt);
+  angleY = kalmanY.getAngle(roll, gyroY, dt);
 }
 
 /**
- * 原点補正後の実施（ボタンを押下した時点を原点に設定）
+ * 原点補正の実施（ボタンを押下した時点を原点に設定）
  */
 void resetOrigin()
 {
   // 原点を引いて相対角度を算出
-  relativeAngleX = filteredAngleX - originAngleX;
-  relativeAngleY = filteredAngleY - originAngleY;
+  relativeAngleX = angleX - originAngleX;
+  relativeAngleY = angleY - originAngleY;
+  relativeAngleZ = angleZ - originAngleZ;
 }
 
 /**
@@ -82,12 +106,16 @@ void resetOrigin()
  */
 void setup()
 {
+  // Z軸（yaw角ドリフト対策）
+  stockedGyroZLength=sizeof(stockedGyroZs)/sizeof(int);
 
   // M5StickC Plusの初期化
   auto cfg = M5.config();
   cfg.internal_imu = true; // 内蔵IMUを有効化
   M5.begin(cfg);
   M5.Imu.init();
+
+  Serial.println(" Dual Button Setup");
 
   // デュアルボタンのPIN情報設定
   pinMode(pin1, INPUT);
@@ -104,12 +132,6 @@ void setup()
   lastUpdate = millis();
 }
 
-int last_value1 = 0;
-int last_value2 = 0;
-
-int cur_value1 = 0;
-int cur_value2 = 0;
-
 /**
  * main / loop
  */
@@ -117,26 +139,41 @@ void loop()
 {
 
   // --------------------------------------------------------------
-  // カルマンフィルタによるジャイロからのX軸、Y軸の傾き取得
+  // カルマンフィルタによるジャイロからのX軸、Y軸の傾き角度取得
 
   // ジャイロデータを取得
   M5.Imu.getGyroData(&gyroX, &gyroY, &gyroZ);
   M5.Imu.getAccelData(&accX, &accY, &accZ);
 
-  // カルマンフィルタを使用して角度を計算
+  // カルマンフィルタを使用してangleX（pitch）, angleY（roll）の角度を計算
   calculateAngle();
+
+  // --------------------------------------------------------------
+  // センサーフュージョンによるZ軸（方位角度）の角度取得：初期値は180
+  // M5Stackのセンサーの既知の問題であるZ軸のドリフトに補正をかけたもの
+
+  // 起動時にstockedGyroZLengthの数だけデータを貯める
+  if(stockCnt<stockedGyroZLength){
+    stockedGyroZs[stockCnt]=gyroZ;
+    stockCnt++;
+  }else{
+    if(adjustGyroZ==0){
+      for(int i=0;i<stockedGyroZLength;i++){
+        adjustGyroZ+=stockedGyroZs[i]/stockedGyroZLength;
+      }
+    }
+    //貯めたデータの平均値を使ってgyroZを補正する
+    gyroZ-=adjustGyroZ; 
+
+    // センサーフュージョンを使用してangleZ（yaw）を取得
+    deltat = fusion.deltatUpdate();
+    //補正したgyroデータを使ってyawを計算
+    fusion.MahonyUpdate(gyroX*DEG_TO_RAD, gyroY*DEG_TO_RAD, gyroZ*DEG_TO_RAD, accX, accY, accZ, deltat);
+    angleZ = fusion.getYaw();
+  }
 
   // 画面に表示する前にクリア
   M5.Lcd.fillScreen(BLACK);
-
-  // ホームボタンを押したか？（1度だけ取得可能）
-  if (M5.BtnA.wasPressed())
-  {
-    M5.Speaker.tone(1500, 100);
-    // ボタンを押した時点の角度を原点として扱うために取得
-    originAngleX = filteredAngleX;
-    originAngleY = filteredAngleY;
-  }
 
   // --------------------------------------------------------------
   // デュアルボタンのイベント取得
@@ -164,8 +201,9 @@ void loop()
       M5.Lcd.setCursor(LEFT_OFFSET_POS, LABEL_POS);
       M5.Lcd.print("Red Button Prsed");
       // ボタンを押した時点の角度を原点として扱うために取得
-      originAngleX = filteredAngleX;
-      originAngleY = filteredAngleY;
+      originAngleX = angleX;
+      originAngleY = angleY;
+      originAngleZ = angleZ;
     }
     last_value2 = cur_value2;
   }
@@ -173,23 +211,27 @@ void loop()
   // ボタンを押した時点の originAngleX, originAngleY を使用して原点をリセット
   resetOrigin();
 
-  // 縦置きで前後回転（ジャイロから計算）
+  // --------------------------------------------------------------
+  // 画面表示
+
+  // 前後に傾ける（ジャイロから計算）
   M5.Lcd.setCursor(LEFT_OFFSET_POS, PARAM_POS);
-  M5.Lcd.printf("Angle X: %d\n", int(filteredAngleX));
+  M5.Lcd.printf("RAW   X: %d\n", int(angleX));
   M5.Lcd.setCursor(LEFT_OFFSET_POS, PARAM_POS + FONT_SIZE*10*1);
-  M5.Lcd.printf("Angle X: %d\n", int(relativeAngleX));
+  M5.Lcd.printf("RESET X: %d\n", int(relativeAngleX));
 
-  // 縦置きで左右に傾ける（ジャイロから計算）
+  // 左右に傾ける（ジャイロから計算）
   M5.Lcd.setCursor(LEFT_OFFSET_POS, PARAM_POS + FONT_SIZE*10*2);
-  M5.Lcd.printf("Angle Y: %d\n", int(filteredAngleY));
+  M5.Lcd.printf("RAW   Y: %d\n", int(angleY));
   M5.Lcd.setCursor(LEFT_OFFSET_POS, PARAM_POS + FONT_SIZE*10*3);
-  M5.Lcd.printf("Angle Y: %d\n", int(relativeAngleY));
+  M5.Lcd.printf("RESET Y: %d\n", int(relativeAngleY));
 
-  // // 縦置きで左右に水平に回転（地磁気から計算）
-  // TODO
-  // M5.Lcd.printf("Angle Z: %d\n", int(relativeAngleZ));
-  // M5.Lcd.printf("Angle Z: %d\n", int(relativeAngleZ));
+  // 左右に水平に回転（地磁気から計算）
+  M5.Lcd.setCursor(LEFT_OFFSET_POS, PARAM_POS + FONT_SIZE*10*4);
+  M5.Lcd.printf("RAW   Z: %d\n", int(angleZ));
+  M5.Lcd.setCursor(LEFT_OFFSET_POS, PARAM_POS + FONT_SIZE*10*5);
+  M5.Lcd.printf("RESET Z: %d\n", int(relativeAngleZ));
 
   M5.update(); // ボタンの状態を更新
-  delay(100);
+  delay(10);
 }
